d57bc197ef5b851c9f0c55f8505fa160ce4de927
Jonathan Lennartz <jlennartz@users.noreply.github.com>
2023-06-22 10:43:19 +0200

    adds top-level doc strings for usability and removes unnecessary comments/functionalities for the model classes

---
+++ src/models/wrapper.py
@@ -0,0 +1,126 @@
+import torch
+from torch import nn, Tensor
+from typing import Iterable, Dict, Callable, Tuple, Union
+from copy import deepcopy
+from model.UNet2D20 import UNet2D
+from losses import KL_loss_VAE
+
+
+class Frankenstein(nn.Module):
+    """Wrapper class for segmentation models and feature transformations.
+
+    Wraps (a copy of) the segmentation model and attaches feature
+    trasformations to it via hooks (at potentially various positions
+    simultaneously). Additionally, it provides control utilities for the
+    hooks as well as different types for inference and training.
+    """
+
+    def __init__(
+        self,
+        seg_model: nn.Module,
+        transformations: nn.ModuleDict,
+        disabled_ids: list[str] = [],
+        copy: bool = True,
+    ):
+        super().__init__()
+        self.seg_model = deepcopy(seg_model) if copy else seg_model
+
+        self.transformations = transformations
+        self.disabled_ids = disabled_ids
+        self.transformation_handles = {}
+        self.train_transformation_handles = {}
+        self.inspect_transformation_handles = {}
+        self.training_data = {}
+        self.inspect_data = {}
+
+    def hook_train_transformations(self, transformations: Dict[str, nn.Module]) -> None:
+        for layer_id in transformations:
+            layer = self.seg_model.get_submodule(layer_id)
+            hook = self._get_train_transformation_hook(
+                transformations[layer_id], layer_id
+            )
+            self.train_transformation_handles[
+                layer_id
+            ] = layer.register_forward_pre_hook(hook)
+
+    def hook_transformations(
+        self, transformations: Dict[str, nn.Module], n_samples: int
+    ) -> None:
+        for layer_id in transformations:
+            layer = self.seg_model.get_submodule(layer_id)
+            hook = self._get_transformation_hook(transformations[layer_id], n_samples)
+            self.transformation_handles[layer_id] = layer.register_forward_pre_hook(
+                hook
+            )
+
+    def _get_train_transformation_hook(
+        self, transformation: nn.Module, layer_id: str
+    ) -> Callable:
+        def hook(module: nn.Module, x: Tuple[Tensor]) -> Tensor:
+            x_in, *_ = x  # tuple, alternatively use x_in = x[0]
+            x_in_new = transformation(x_in)
+            if layer_id not in self.disabled_ids:
+                mse = nn.functional.mse_loss(x_in_new, x_in.detach(), reduction="mean")
+
+                training_data = {
+                    "mse": mse,
+                }
+
+                self.training_data[layer_id] = training_data
+
+            return x_in_new
+
+        return hook
+
+    def _get_transformation_hook(
+        self, transformation: nn.Module, n_samples: int = 1
+    ) -> Callable:
+        def hook(module: nn.Module, x: Tuple[Tensor]) -> Tensor:
+            x_in, *_ = x  # weird tuple, can use x_in = x[0]
+            if n_samples == 0:
+                return x
+            elif n_samples == -1:
+                x_in_new = transformation(x_in)
+                return x_in_new
+            else:
+                x_in_new = x_in.unsqueeze(1).repeat(1, n_samples, 1, 1, 1).flatten(0, 1)
+                x_in_new = transformation(x_in_new)
+                return torch.cat([x_in, x_in_new], dim=0)
+
+        return hook
+
+    def remove_train_transformation_hook(self, layer_id: str) -> None:
+        self.train_transformation_handles[layer_id].remove()
+
+    def remove_transformation_hook(self, layer_id: str) -> None:
+        self.transformation_handles[layer_id].remove()
+
+    def remove_all_hooks(self):
+        if hasattr(self, "train_transformation_handles"):
+            for handle in self.train_transformation_handles:
+                self.train_transformation_handles[handle].remove()
+            self.train_transformation_handles = {}
+
+        if hasattr(self, "transformation_handles"):
+            for handle in self.transformation_handles:
+                self.transformation_handles[handle].remove()
+            self.transformation_handles = {}
+
+    def freeze_seg_model(self):
+        self.seg_model.eval()
+        for param in self.seg_model.parameters():
+            param.requires_grad = False
+
+    def set_number_of_samples_to(self, n_samples: int):
+        self.n_samples = n_samples
+
+    def disable(self, layer_ids: list) -> None:
+        for layer_id in layer_ids:
+            self.transformations[layer_id].turn_off()
+
+    def enable(self, layer_ids: list) -> None:
+        for layer_id in layer_ids:
+            self.transformations[layer_id].turn_on()
+
+    def forward(self, x: Tensor):
+        return self.seg_model(x)
