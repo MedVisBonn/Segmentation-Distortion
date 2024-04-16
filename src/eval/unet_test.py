5a078e0968845e480348b0d2211f6c91dd99daa2
jlennartz <jlennartz@proton.me>
2024-04-16 20:19:34 +0000

    adds generator support for unet eval

--- src/eval/unet_test.py
+++ src/eval/unet_test.py
@@ -1,11 +1,12 @@
 
-from typing import Dict
+from typing import Dict, Union
 from omegaconf import OmegaConf
 import pandas as pd
 import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
+from batchgenerators.dataloading.multi_threaded_augmenter import MultiThreadedAugmenter
 
 from utils import  epoch_average
 from data_utils import volume_collate
@@ -41,41 +42,75 @@ def get_df_from_dict(
 def eval_set(
     cfg: OmegaConf,
     model: nn.Module,
-    dataset: Dataset
+    data: Union[Dataset, MultiThreadedAugmenter],
+    loader_format: str = 'set',
 ) -> Dict:
     if cfg.run.data_key == 'brain':
-        dataset.volume_wise = True
-        dataloader = DataLoader(
-            dataset,
-            batch_size=1,
-            shuffle=False,
-            drop_last=False,
-            collate_fn=volume_collate
-        )
+
         eval_metrics = {
             'Volumetric Dice': DiceScoreCalgary(),
-            'Surface Dice': SurfaceDiceCalgary()
         }
-        metrics = eval_brain_set(
-            model=model, 
-            dataloader=dataloader, 
-            eval_metrics=eval_metrics
-        )
+
+        if loader_format == 'set':
+            eval_metrics['Surface Dice'] = SurfaceDiceCalgary()
+            data.volume_wise = True
+            dataloader = DataLoader(
+                data,
+                batch_size=1,
+                shuffle=False,
+                drop_last=False,
+                collate_fn=volume_collate
+            )        
+            metrics = eval_brain_set(
+                model=model, 
+                dataloader=dataloader, 
+                eval_metrics=eval_metrics
+            )
+
+        elif loader_format == 'generator':
+            metrics = eval_brain_generator(
+                model=model, 
+                generator=data, 
+                n_batches=250,
+                eval_metrics=eval_metrics
+            )
+        else:
+            raise ValueError(
+                f'Invalid loader format. No loader format named {loader_format}.'
+                f'Choose from "set" or "generator".'
+            )
 
     elif cfg.run.data_key == 'heart':
-        dataloader = DataLoader(dataset, 
-            batch_size=32, 
-            shuffle=False, 
-            drop_last=False
-        )
+
         eval_metrics = {
             "Volumetric Dice": DiceScoreMMS()
         }
-        metrics = eval_heart_set(
-            model=model, 
-            dataloader=dataloader, 
-            eval_metrics=eval_metrics
-        )
+
+        if loader_format == 'set':
+            dataloader = DataLoader(
+                data, 
+                batch_size=32, 
+                shuffle=False, 
+                drop_last=False
+            )
+            metrics = eval_heart_set(
+                model=model, 
+                dataloader=dataloader, 
+                eval_metrics=eval_metrics
+            )
+
+        elif loader_format == 'generator':
+            metrics = eval_heart_generator(
+                model=model, 
+                generator=data, 
+                n_batches=250,
+                eval_metrics=eval_metrics
+            )
+        else:
+            raise ValueError(
+                f'Invalid loader format. No loader format named {loader_format}.'
+                f'Choose from "set" or "generator".'
+            )
     
     else:
         raise ValueError(
@@ -116,6 +151,41 @@ def eval_brain_set(
     return epoch_metrics
 
 
+@torch.no_grad()
+def eval_brain_generator(
+    model: nn.Module, 
+    generator: MultiThreadedAugmenter, 
+    n_batches: int,
+    eval_metrics: Dict
+) -> Dict:
+    model.eval()
+    batch_sizes = []
+    epoch_metrics = {key: [] for key in eval_metrics.keys()}
+    for it in range(n_batches):
+        batch = next(generator)
+        input_ = batch['input']
+        target = batch['target']
+        # batch_sizes.append(input_.shape[0])
+
+        # input_chunks  = torch.split(input_, 32, dim=0)
+        # net_out = []
+        # for input_chunk in input_chunks:
+        #     net_out_chunk = model(input_chunk.cuda())
+        #     net_out.append(net_out_chunk.detach().cpu())
+
+        # net_out = torch.cat(net_out, dim=0)
+
+        batch_sizes.append(input_.shape[0])
+        net_out = model(input_.cuda())
+        for key, metric in eval_metrics.items():
+            epoch_metrics[key].append(metric(net_out,target).detach().mean().cpu())
+
+    for key, epoch_scores in epoch_metrics.items():
+        epoch_metrics[key] = epoch_average(epoch_scores, batch_sizes)
+
+    return epoch_metrics
+
+
 
 @torch.no_grad()
 def eval_heart_set(
@@ -129,6 +199,45 @@ def eval_heart_set(
     batch_sizes = []
     for batch in dataloader:
         input_ = batch['input']
+        target = batch['target'].cuda()
+        # convert -1 labels to background
+        target[target == -1] = 0
+        # convert to one-hot encoding
+        target = F.one_hot(
+            target.long(), 
+            num_classes=4
+        ).squeeze(1).permute(0,3,1,2)
+        # get model output
+        net_out = model(input_.cuda())
+        
+        batch_sizes.append(input_.shape[0])
+        for key, metric in eval_metrics.items():
+            epoch_metrics[key].append(
+                metric(net_out, target).detach().mean().cpu()
+            )
+            
+    for key, epoch_scores in epoch_metrics.items():
+        epoch_metrics[key] = epoch_average(epoch_scores, batch_sizes)
+        
+    return epoch_metrics
+
+
+
+@torch.no_grad()
+def eval_heart_generator(
+    model: nn.Module, 
+    generator: MultiThreadedAugmenter,
+    n_batches: int,
+    eval_metrics: Dict
+) -> Dict:
+    model.eval()
+    epoch_metrics = {key: [] for key in eval_metrics.keys()}
+    # saves batch sizes for each batch for averaging
+    batch_sizes = []
+
+    for it in range(n_batches):
+        batch = next(generator)
+        input_ = batch['data']
         target = batch['target'].cuda()
         # convert -1 labels to background
         target[target == -1] = 0
